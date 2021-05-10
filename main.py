05a5e9df0a76a0e474457dc9ce8fd0158605e718
JhosefMatheus <jhosef.mat123@hotmail.com>
2021-05-10 08:40:24 -0300

    adicionando frame_a, responsável por receber a tela que exibirá os cálculos, e o frame_b, responsável por receber todos os botões da calculadora.

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Calculadora')
 window.geometry('500x500')
+window.resizable(False, False)
+
+frame_a = tk.Frame(master=window, width=500, background='white')
+frame_b = tk.Frame()
+
+label = tk.Label(master=frame_a, text='0', bg='white')
+label.pack()
+
+frame_a.pack()
+frame_b.pack()
 
 window.mainloop()
