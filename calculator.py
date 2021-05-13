5c6aef1560d0f2285a0d3610024643071c6a8db5
JhosefMatheus <jhosef.mat123@hotmail.com>
2021-05-12 23:44:45 -0300

    criando a tela da calculadora e validando as expressões e retornando as mesmas. OBS: ainda há muito a melhorar com relação às operaçãoes aritméticas.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,51 @@
 class Calculator:
     def __init__(self):
         self.__screen = []
+        self.__operations = ['+', '-', 'x', '/', '\u221a', 'x²', '1/x', '%', 'CE', 'C', '-/+', '=', '\u2190']
 
-    def button_click(self, button):
-        print(button)
+    def button_click(self, button, label):
+        if label['text'] == '0' and button == '0':
+            print(f'screen: {self.__screen}')
+            pass
+
+        elif label['text'] == '0' and button == '.':
+            self.__screen.append('0.')
+            print(f'screen: {self.__screen}')
+        
+        elif button not in self.__operations and len(self.__screen) == 0:
+            self.__screen.append(button)
+            label['text'] = self.__screen[0]
+            print(f'screen: {self.__screen}')
+        
+        elif button not in self.__operations and len(self.__screen) == 1:
+            n = self.__screen[0] + button
+            self.__screen[0] = n
+            label['text'] = self.__screen[0]
+            print(f'screen: {self.__screen}')
+        
+        elif button in self.__operations and len(self.__screen) == 0:
+            print(f'screen: {self.__screen}')
+            pass
+
+        elif button in self.__operations and len(self.__screen) == 1:
+            self.__screen.append(button)
+            label['text'] = '0'
+            print(f'screen: {self.__screen}')
+        
+        elif button not in self.__operations and len(self.__screen) == 2:
+            self.__screen.append(button)
+            label['text'] = self.__screen[2]
+            print(f'screen: {self.__screen}')
+        
+        elif button not in self.__operations and len(self.__screen) == 3:
+            n = self.__screen[2] + button
+            self.__screen[2] = n
+            label['text'] = self.__screen[2]
+            print(f'screen: {self.__screen}')
+
+        elif button in self.__operations and len(self.__screen) == 3:
+            n = eval(f'{self.__screen[0]} {self.__screen[1]} {self.__screen[2]}')
+            print(n)
+            self.__screen = [str(n)]
+            label['text'] = self.__screen[0]
+            print(f'screen: {self.__screen}')
--- main.py
+++ main.py
@@ -22,83 +22,90 @@ window.geometry(f'{w}x{h}+{int(position_x)}+{int(position_y)}')
 window.resizable(False, False)
 window['bg'] = '#808080'
 
+# !labels
+label_screen = tk.Label(master=window,
+                        text='0',
+                        font='Arial 20')
+
+label_screen.pack()
+
 # !botões
 btn_0 = tk.Button(master=window, 
                 text='0', 
-                command=lambda:calculator.button_click(btn_0.cget('text')))
+                command=lambda:calculator.button_click(btn_0.cget('text'), label_screen))
 
 btn_1 = tk.Button(master=window, 
                 text='1',
-                command=lambda:calculator.button_click(btn_1.cget('text')))
+                command=lambda:calculator.button_click(btn_1.cget('text'), label_screen))
 
 btn_2 = tk.Button(master=window, 
                 text='2',
-                command=lambda:calculator.button_click(btn_2.cget('text')))
+                command=lambda:calculator.button_click(btn_2.cget('text'), label_screen))
 
 btn_3 = tk.Button(master=window, 
                 text='3',
-                command=lambda:calculator.button_click(btn_3.cget('text')))
+                command=lambda:calculator.button_click(btn_3.cget('text'), label_screen))
 
 btn_4 = tk.Button(master=window, 
                 text='4',
-                command=lambda:calculator.button_click(btn_4.cget('text')))
+                command=lambda:calculator.button_click(btn_4.cget('text'), label_screen))
 
 btn_5 = tk.Button(master=window, 
                 text='5',
-                command=lambda:calculator.button_click(btn_5.cget('text')))
+                command=lambda:calculator.button_click(btn_5.cget('text'), label_screen))
 
 btn_6 = tk.Button(master=window, 
                 text='6',
-                command=lambda:calculator.button_click(btn_6.cget('text')))
+                command=lambda:calculator.button_click(btn_6.cget('text'), label_screen))
 
 btn_7 = tk.Button(master=window, 
                 text='7',
-                command=lambda:calculator.button_click(btn_7.cget('text')))
+                command=lambda:calculator.button_click(btn_7.cget('text'), label_screen))
 
 btn_8 = tk.Button(master=window, 
                 text='8',
-                command=lambda:calculator.button_click(btn_8.cget('text')))
+                command=lambda:calculator.button_click(btn_8.cget('text'), label_screen))
 
 btn_9 = tk.Button(master=window, 
                 text='9',
-                command=lambda:calculator.button_click(btn_9.cget('text')))
+                command=lambda:calculator.button_click(btn_9.cget('text'), label_screen))
 
 btn_equal = tk.Button(master=window, 
                     text='=',
-                    command=lambda:calculator.button_click(btn_equal.cget('text')))
+                    command=lambda:calculator.button_click(btn_equal.cget('text'), label_screen))
 btn_sum = tk.Button(master=window, 
                     text='+',
-                    command=lambda:calculator.button_click(btn_sum.cget('text')))
+                    command=lambda:calculator.button_click(btn_sum.cget('text'), label_screen))
 btn_dif = tk.Button(master=window, 
                     text='-',
-                    command=lambda:calculator.button_click(btn_dif.cget('text')))
+                    command=lambda:calculator.button_click(btn_dif.cget('text'), label_screen))
 btn_prod = tk.Button(master=window, 
                     text='x',
-                    command=lambda:calculator.button_click(btn_prod.cget('text')))
+                    command=lambda:calculator.button_click(btn_prod.cget('text'), label_screen))
 btn_div = tk.Button(master=window, 
                     text='/',
-                    command=lambda:calculator.button_click(btn_div.cget('text')))
+                    command=lambda:calculator.button_click(btn_div.cget('text'), label_screen))
 btn_sqrt = tk.Button(master=window, 
                     text=u'\u221a',
-                    command=lambda:calculator.button_click(btn_sqrt.cget('text')))
+                    command=lambda:calculator.button_click(btn_sqrt.cget('text'), label_screen))
 btn_pow = tk.Button(master=window, 
                     text='x²',
-                    command=lambda:calculator.button_click(btn_pow.cget('text')))
+                    command=lambda:calculator.button_click(btn_pow.cget('text'), label_screen))
 btn_frac = tk.Button(master=window, 
                     text='1/x',
-                    command=lambda:calculator.button_click(btn_frac.cget('text')))
+                    command=lambda:calculator.button_click(btn_frac.cget('text'), label_screen))
 btn_percent = tk.Button(master=window, 
                         text='%',
-                        command=lambda:calculator.button_click(btn_percent.cget('text')))
+                        command=lambda:calculator.button_click(btn_percent.cget('text'), label_screen))
 btn_ce = tk.Button(master=window, 
                 text='CE',
-                command=lambda:calculator.button_click(btn_ce.cget('text')))
+                command=lambda:calculator.button_click(btn_ce.cget('text'), label_screen))
 btn_c = tk.Button(master=window, 
                 text='C',
-                command=lambda:calculator.button_click(btn_c.cget('text')))
+                command=lambda:calculator.button_click(btn_c.cget('text'), label_screen))
 btn_backspace = tk.Button(master=window, 
                         text=u'\u2190',
-                        command=lambda:calculator.button_click(btn_backspace.cget('text')))
+                        command=lambda:calculator.button_click(btn_backspace.cget('text'), label_screen))
 btn_negate = tk.Button(master=window, 
                     text='-/+',
                     command=lambda:calculator.button_click())
